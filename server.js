3ccb405858a99896e318fa5a06a303f212a4edaa
shohedul350 <shohedulislamemon@gmail.com>
2020-03-11 18:11:05 +0600

    complete

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 //  user router
+app.get('/', (req, res) => res.send('hello'));
 app.use('api/user', require('./routes/user'));
 
 const PORT = process.env.PORT || 5000;
